b81bb388a66462715708468b6c06a9b6c4f8b2d6
AdriFC <adri86vigo@gmail.com>
2021-07-29 17:46:09 +0200

    RxJs SwitchMap, pantalla de ver país. Fin de sección 8

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
+
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,19 +14,29 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor( private activatedRoute: ActivatedRoute, private paisService: PaisService ) { }
 
   ngOnInit(): void {
 
     this.activatedRoute.params
-      .subscribe( ({ id }) => {
-        console.log(id);
+      .pipe(
+        switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ) ),
+        tap ( console.log )
+      )
+      .subscribe( pais => this.pais = pais );
+        
 
-        this.paisService.getPaisPorAlpha( id )
-          .subscribe( pais => {
-            console.log(pais);
-          })
-      })
+    // this.activatedRoute.params
+    //   .subscribe( ({ id }) => {
+    //     console.log(id);
+
+    //     this.paisService.getPaisPorAlpha( id )
+    //       .subscribe( pais => {
+    //         console.log(pais);
+    //       });
+    //   });
 
   }
 
