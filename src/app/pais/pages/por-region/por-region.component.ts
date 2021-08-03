f4e1e35f5993457a39571c265a8d453f0955529a
AdriFC <adri86vigo@gmail.com>
2021-08-03 17:32:31 +0200

    Mostrar países por región

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -15,17 +17,23 @@ export class PorRegionComponent  {
 
   regiones: string [] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
   regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor( private paisService: PaisService ) { }
 
   getClaseCSS( region: string ): string{
     return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
   }
 
   activarRegion( region: string ){
-    this.regionActiva = region;
 
-    //TODO: hacer el llamado al servicio
+    if ( region === this.regionActiva ) { return; }
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarRegion( region )
+      .subscribe( paises => this.paises = paises );
   }
 
 
